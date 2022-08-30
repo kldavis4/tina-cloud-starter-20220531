861dc2ad7a922d800881df5f1f1e8b75a1898240
Kelly Davis <kelly.davis@forestry.io>
2022-08-30 15:07:50 -0400

    updates

--- .tina/schema.ts
+++ .tina/schema.ts
@@ -433,10 +433,10 @@ const schema = defineSchema({
 });
 
 const branch = "main";
-const apiURL =
-  process.env.NODE_ENV == "development"
-    ? "http://localhost:4001/graphql"
-    : `https://content.tinajs.io/content/${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/github/${branch}`;
+// const apiURL =
+//   process.env.NODE_ENV == "development"
+//     ? "http://localhost:4001/graphql"
+//     : `https://content.tinajs.io/content/${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/github/${branch}`;
 
 export const tinaConfig = defineConfig({
   branch: 'main',
